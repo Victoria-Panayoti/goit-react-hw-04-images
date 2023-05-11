8c539a31fb26a7c6bd00cd11a5da9030b2b6c913
Victoria Panajoti <112144156+Victoria-Panayoti@users.noreply.github.com>
2023-05-11 18:04:09 +0300

    made hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Searchbar } from './Searchbar/Searchbar';
@@ -8,76 +7,58 @@ import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout/Layout';
+import { useEffect, useState } from 'react';
 
-export class App extends Component {
-  state = {
-    gallery: [],
-    query: '',
-    page: 1,
-    isLoading: false,
-    loadMore: false,
-    error: null,
-  };
+export const App = () => {
+  const [gallery, setGallery] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
-  handleFormSubmit = ({ searchQuery }) => {
-    const { query, page } = this.state;
+  const handleFormSubmit = ({ searchQuery }) => {
     if (query === searchQuery && page === 1) return;
-    this.setState({
-      query: searchQuery.toLowerCase(),
-      gallery: [],
-      page: 1,
-      loadMore: false,
-    });
+    setGallery([]);
+    setQuery(searchQuery.toLowerCase());
+    setPage(1);
   };
-  componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.getGallery();
+  useEffect(() => {
+    if (!query) {
+      return;
     }
-  }
-  getGallery = async () => {
-    const { query, page } = this.state;
-    this.setState({ isLoading: true });
-    try {
-      const responce = await fetchGallery(query, page);
-      const gallery = responce.hits;
-      const total = responce.totalHits;
-      if (total === 0) {
-        toast.warning('havent found anything, try smthg else.');
-        return;
+    const getGallery = async () => {
+      setIsLoading(true);
+      try {
+        const responce = await fetchGallery(query, page);
+        const gallery = responce.hits;
+        const total = responce.totalHits;
+        if (total === 0) {
+          toast.warning('havent found anything, try smthg else.');
+          return;
+        }
+        setGallery(prevGallery => [...prevGallery, ...gallery]);
+      } catch (error) {
+        toast.error(`Smthg went wrong, we re so sorry.`);
+      } finally {
+        setIsLoading(false);
       }
-      this.setState(state => ({
-        gallery: [...state.gallery, ...gallery],
-      }));
-    } catch (error) {
-      toast.error(`Smthg went wrong, we re so sorry. ${error.message}`);
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-  render() {
-    const { gallery, isLoading,error } = this.state;
-    return (
-      <Layout>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ToastContainer autoClose={3000} theme="colored" pauseOnHover />
-        {error && (
-          <h1 style={{ color: 'orangered', textAlign: 'center' }}>
-            {error.message}
-          </h1>
-        )}
-        {isLoading && <Loader />}
-        {gallery.length > 0 && <ImageGallery gallery={gallery} />}
-        {!isLoading && gallery.length > 11 && (
-          <Button onClick={this.loadMore} />
-        )}
-        <GlobalStyle/>
-      </Layout>
-    );
-  }
-}
+    };
+    getGallery();
+  }, [query, page]);
+
+  return (
+    <Layout>
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ToastContainer autoClose={3000} theme="colored" pauseOnHover />
+      {isLoading && <Loader />}
+      {gallery.length > 0 && <ImageGallery gallery={gallery} />}
+      {!isLoading && gallery.length > 11 && (
+        <Button
+          onClick={() => {
+            setPage(prevPage => prevPage + 1);
+          }}
+        />
+      )}
+      <GlobalStyle />
+    </Layout>
+  );
+};
